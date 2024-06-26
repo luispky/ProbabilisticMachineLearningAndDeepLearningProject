0f49dd56b3491b42cf95b1521348ccce930a9f1c
luispky <luispalaciosky@gmail.com>
2024-06-26 17:28:44 +0200

    Run file modification for DDPM

--- scripts/run_diffusion.py
+++ scripts/run_diffusion.py
@@ -18,21 +18,36 @@ def main():
     # define the arguments
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
+    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    num_classes = 2
+    beta_ema = 0.995
+    args.samples = 1000
+    args.cfg_strength = 2.0
+    time_dim_embedding = 100
+    
+    noise_time_steps = 100
     args.epochs = 50
     args.lr = 1e-3
-    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    args.samples = 1000
+    
+    with_labels = False
+    image_name = 'generated_samples_'+'24'
+    
+    save_model = False
+    model_name = 'ddpm_model_' + '03'
     
     # define the components of the DDPM model: dataset, scheduler, model, EMA class
+    ema = EMA(beta=beta_ema)
     dataset = Dataset()
-    # dataloader = dataset.generate_data(with_labels=True)
-    dataloader = dataset.generate_data(with_labels=False)
+    
+    
+    if with_labels:
+        dataloader = dataset.generate_data(with_labels=True)
+    else:
+        dataloader = dataset.generate_data(with_labels=False)
+    
     dataset_shape = dataset.get_dataset_shape()
-    noise_time_steps = 100
     scheduler = LinearNoiseScheduler(noise_timesteps=noise_time_steps, dataset_shape=dataset_shape)
-    time_dim_embedding = 100
-    model = NoisePredictor(dataset_shape = dataset_shape, time_dim=time_dim_embedding, num_classes=2)
-    ema = EMA(beta=0.995)
+    model = NoisePredictor(dataset_shape = dataset_shape, time_dim=time_dim_embedding, num_classes=num_classes)
     
     # Instantiate the DDPM model
     diffusion = DDPM(scheduler, model, args)
@@ -41,19 +56,25 @@ def main():
     diffusion.train(dataloader, ema)
     
     # save model
-    # diffusion.save_model(diffusion.model, 'ddpm_model_03')
+    if save_model:
+        diffusion.save_model(diffusion.model, model_name)
     
     # generate samples and 
     # bring labels and samples to the cpu
-    # labels = torch.randint(0, 2, (args.samples,)).to(args.device)
-    # labels = labels.cpu().numpy()
-    labels = None
-    samples = diffusion.sample(diffusion.model, labels)
+    if with_labels:
+        labels = torch.randint(0, num_classes, (args.samples,)).to(args.device)
+        labels = labels.cpu().numpy()
+    else:
+        labels = None
+        
+    samples = diffusion.sample(diffusion.model, labels, args.cfg_strength)
     samples = samples.cpu().numpy()
     
     # save the generated samples
-    # save_plot_generated_samples('generated_samples_19', samples, labels=None)
-    save_plot_generated_samples('generated_samples_23', samples, labels=None) 
+    if with_labels:
+        save_plot_generated_samples(image_name, samples, labels=labels)
+    else:   
+        save_plot_generated_samples(image_name, samples, labels=None) 
 
 def test():
     dataset = Dataset()
