4d18435aff9a1cc7b71f0a26f5b7efddbe7b4bec
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2024-06-29 12:44:43 +0200

    requirements.txt

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib
 import torch
+import wandb
 
 
 print(f'numpy {np.__version__}')
 print(f'matplotlib {matplotlib.__version__}')
 print(f'torch {torch.__version__}')
+print(f'wandb {wandb.__version__}')
 
-# numpy 1.26.3
-# matplotlib 3.8.2
-# torch 2.3.0+cpu
+# numpy 2.0.0
+# matplotlib 3.9.0
+# torch 2.3.1+cpu
