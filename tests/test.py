bc6ddf14da44d1c53362a64d35a614df2f1a9490
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2024-06-29 12:10:19 +0200

    tests added

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib
+import torch
+
+
+print(f'numpy {np.__version__}')
+print(f'matplotlib {matplotlib.__version__}')
+print(f'torch {torch.__version__}')
+
+# numpy 1.26.3
+# matplotlib 3.8.2
+# torch 2.3.0+cpu
