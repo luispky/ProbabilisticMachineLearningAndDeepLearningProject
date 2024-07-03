8ad25a58959cbb88f73c7ae627b71bddd5746940
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2024-07-03 20:27:43 +0200

    Update anomaly_correction_example.py

--- tests/anomaly_correction_example.py
+++ tests/anomaly_correction_example.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 from src.anomaly_correction import AnomalyCorrection
+from src.datasets import DatabaseInterface
+from src.inverse_gradient import InverseGradient
 
 
 def main(data_path='..\\datasets\\sample_data_preprocessed.csv',
@@ -9,9 +11,10 @@ def main(data_path='..\\datasets\\sample_data_preprocessed.csv',
     Example of the AnomalyCorrection class
     """
     """ 1/2)
-    Your colleague has given you a clean dataset with examples
-    of normal data-points and anomalies. He wants you to train 
-    a model that is able to classify the anomalies. 
+    Your colleague has given you a clean dataset of tabular 
+    discrete-valued features with examples of normal data-points 
+    and anomalies. He wants you to train a model that is able to 
+    classify the anomalies. 
     """
     # get data
     df_x = pd.read_csv(data_path)
@@ -21,18 +24,18 @@ def main(data_path='..\\datasets\\sample_data_preprocessed.csv',
     df_y = df_x.copy()['Response_1']
     del df_x['Response_1']
 
-    # train/load the model
-
-    if os.path.exists(model_path):
-        ac = AnomalyCorrection(model_name=DEFAULT_MODEL_NAME)
+    # move data to probability space
+    data_x = DatabaseInterface(df_x)
 
     """ 2/2)
     Then, your colleague passes you examples of data-points that
-    a system classified as  anomalies. You want to correct these 
-    anomalies, so that they look more like normal data-points. 
-    This will help your colleague in understanding why the system
-    classified them as anomalies.  
+    a system classified as anomalies. You want to correct them, 
+    so that they look more like normal data-points. This will 
+    help your colleague in understanding why the system classified 
+    them as anomalies.  
     """
-    ac = AnomalyCorrection()
+    ...
 
 
+if __name__ == '__main__':
+    main()
